2c1ae1f4688ec5467d7762c6124ddb9db7c532b8
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-01 10:50:40 -0300

    Integração Firebase com React

---
+++ firebase/config.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBNNjK292KIxT__iCMXXBmRyddXzwv6gfg",
+    authDomain: "miniblog-f18ba.firebaseapp.com",
+    projectId: "miniblog-f18ba",
+    storageBucket: "miniblog-f18ba.firebasestorage.app",
+    messagingSenderId: "767633795255",
+    appId: "1:767633795255:web:aecb8643c5b64467090532"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db }; // for import with keys, like: ( import { db } from '...' )
+export default db; // for import with no keys, like: ( import db from '...' )
