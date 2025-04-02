c4044ad3bf32523b30509f658c4b45c3b905310c
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-02 10:59:30 -0300

    Validação de cadastro + vinculação de states com fórm

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,26 +5,65 @@ import styles from './Register.module.css'
 import { useState, useEffect } from 'react';
 
 const Register = () => {
+    const [ displayName, setDisplayName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ confirmPassword, setConfirmPassword ] = useState('');
+    const [ error, setError ] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const user = {
+            displayName, email, password, confirmPassword
+        }
+
+        if(password != confirmPassword){
+            setError(true);
+            return
+        }
+
+        console.log(user);
+    };
+
+    useEffect(() => {
+        if(error){
+            const time = setTimeout(() => {
+                setError(false)
+            }, 3000);
+
+            // Se error mudar antes do tempo acabar, o clearTimeout evita múltiplos timers.
+            return () => clearTimeout(time); // Limpa o timer ao desmontar o componente
+        }
+    }, [ error ]);
+
     return (
-        <div>
+        <div className={ styles.register }>
             <h1>Cadastre-se para postar</h1>
             <p>Crie seu usuário e compartilhe suas histórias</p>
-            <form>
+
+            <form onSubmit={handleSubmit}>
+                {error && <p className='error'>As senhas precisam ser iguais</p>}            
+                
                 <label>
                     <span>Nome: </span>
-                    <input type="text" name='displayName' required placeholder='Nome de usuário' />
+                    <input value={displayName} type="text" name='displayName' required placeholder='Nome de usuário' 
+                    onChange={(e) => setDisplayName(e.target.value)} />
                 </label>
                 <label>
                     <span>Email: </span>
-                    <input type="email" name='email' required placeholder='E-mail de usuário' />
+                    <input value={email} type="email" name='email' required placeholder='E-mail de usuário' 
+                    onChange={(e) => setEmail(e.target.value)} />
                 </label>
                 <label>
                     <span>Senha: </span>
-                    <input type="password" name='password' required placeholder='Insira sua senha' />
+                    <input value={password} type="password" name='password' required placeholder='Insira sua senha' 
+                    onChange={(e) => setPassword(e.target.value)}/>
                 </label>
                 <label>
                     <span>Confirmação de senha: </span>
-                    <input type="password" name='confirmPassword' required placeholder='Confirme sua senha' />
+                    <input value={confirmPassword} type="password" name='confirmPassword' required placeholder='Confirme sua senha' 
+                    onChange={(e) => setConfirmPassword(e.target.value)} />
                 </label>
                 <button className='btn'>Cadastrar</button>
             </form>
