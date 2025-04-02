7069b13ee1edda7f519b5b873e9126159c986789
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-02 10:26:03 -0300

    Formulário de cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import NavBar from './components/Navbar/NavBar'
 // import pages
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 
 // import style
 import './App.css'
@@ -21,6 +23,8 @@ function App() {
                     <Routes>
                         <Route path='/' element={ <Home /> } />
                         <Route path='/about' element={ <About /> } />
+                        <Route path='/login' element={ <Login /> } />
+                        <Route path='/register' element={ <Register /> } />
                     </Routes>
                 </div>
 
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,35 @@
+// css
+import styles from './Register.module.css'
+
+// modules
+import { useState, useEffect } from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h1>Cadastre-se para postar</h1>
+            <p>Crie seu usuário e compartilhe suas histórias</p>
+            <form>
+                <label>
+                    <span>Nome: </span>
+                    <input type="text" name='displayName' required placeholder='Nome de usuário' />
+                </label>
+                <label>
+                    <span>Email: </span>
+                    <input type="email" name='email' required placeholder='E-mail de usuário' />
+                </label>
+                <label>
+                    <span>Senha: </span>
+                    <input type="password" name='password' required placeholder='Insira sua senha' />
+                </label>
+                <label>
+                    <span>Confirmação de senha: </span>
+                    <input type="password" name='confirmPassword' required placeholder='Confirme sua senha' />
+                </label>
+                <button className='btn'>Cadastrar</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
