4c729d0488bde72b3fa0338c011dad26e89b850a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-03 11:35:43 -0300

    Bloq. para acessos não autenticados + estrutura/estilo da page about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,10 +2,17 @@
 // css
 import styles from './About.module.css';
 
+// modules
+import { Link } from 'react-router-dom';
+
 const About = () => {
     return (
-        <div>
-            <h1 className={styles.teste}>About</h1>
+        <div className={ styles.about }>
+            <h2>Sobre o Mini <span>Blog</span></h2>
+            <p>Este projeto consiste em um Blog feito com React no Front-end e Firebase no Back-end</p>
+            <Link to='/posts/create' className='btn'>
+                Criar Post
+            </Link>
         </div>
     );
 };
