5bdc0abcbcb4ebf119b60d2b9bf612c25b639529
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-07 09:01:03 -0300

    Exibição de resultados de busca

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -2,17 +2,34 @@
 // css
 import styles from './Search.module.css';
 
+// components
+import PostDetail from '../../components/PostDetail';
+
 // hooks
 import { useQuery } from '../../hooks/useQuery'; // custom hook to return url params
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { Link } from 'react-router-dom';
+
 
 const Search = () => {
     const query = useQuery();
     const search = query.get("q"); // get() is a method from "URLSearchParams()" by custom hook for get params
+    const { documents: posts } = useFetchDocuments('posts', search);
 
     return (
-        <div>
+        <div className={ styles.search_container }>
             <h1>Search</h1>
-            <p>{ search }</p>
+            <div>
+                { posts && posts.length === 0 && (
+                    <>
+                        <p>Não foram encontrados posts a partir da sua busca...</p>
+                        <Link to='/' className='btn btn-dark'>Voltar</Link>
+                    </>
+                ) }
+                { posts && posts.map((post) => (
+                    <PostDetail key={ post.id } post={ post } />
+                )) }
+            </div>
         </div>
     );
 };
