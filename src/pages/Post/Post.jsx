da4c25562fbebed85696b9bbd66ffe41eed60362
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-09 09:21:29 -0300

    Estilização de página de post individual

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -12,14 +12,24 @@ const Post = () => {
     const { document: post, loading, error } = useFetchPost('posts', id);
 
     return (
-        <div>
+        <div className={ styles.post_container }>
             { loading && <p>Carregando Post...</p> }
             { error && <p>{ error }</p> }
             { post && (
+                // it can use 'postDetail.jsx' component...
                 <>
                     <h1>{ post.title }</h1>
-                    <img src={ post.image } alt="" />
-                    <p>Criado por: { post.createdBy }</p>
+                    <img src={ post.image } alt={ post.title } />
+                    <p>{ post.body }</p>
+                    <h3>Este post trata sobre: </h3>
+                    <div className={ styles.tags }>
+                        { post.tags.map((tag) => (
+                            <p key={ tag }>
+                                <span>#</span>
+                                { tag }
+                            </p>
+                        )) }
+                    </div>
                 </>
             ) }
         </div>
