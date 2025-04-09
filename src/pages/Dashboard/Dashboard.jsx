fb525c3818d01f6b02d2e237fbab673ce88f2ae6
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-09 09:35:18 -0300

    Estruturando a dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,10 +2,36 @@
 // css
 import styles from './Dashboard.module.css';
 
+// components
+import { Link } from 'react-router-dom';
+
+// contexts
+import { useAuthValue } from '../../context/AuthContext'; // custom hook
+
+// hooks
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'; // custom hook
+
 const Dashboard = () => {
+    const { user } = useAuthValue();
+    const userID = user.uid
+    const posts = [];
+
+    //const { documents: posts } = useFetchDocuments('posts', userID)
+
     return (
         <div>
-            <h1>DashBoard</h1>        
+            <h2>DashBoard</h2>        
+            <p>Gerencie os seus posts</p>
+            { posts && posts.length === 0 ? (
+                <div className={ styles.noposts }>
+                    <p>Não foram encontrados posts...</p>
+                    <Link to='/posts/create' className='btn'>Criar primeiro post</Link>
+                </div>
+            ) : (
+                <div>
+                    <p>Possui posts...</p>
+                </div>
+            )}
         </div>
     );
 };
